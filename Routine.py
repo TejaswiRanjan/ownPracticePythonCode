ae89b153088af126e303f13b753fcc37899acdb1
TejaswiRanjan <ranjantejaswi65@gmail.com>
2024-03-24 23:19:01 +0530

    This is my personal routine code

---
+++ Routine.py
@@ -0,0 +1,109 @@
+#My personal Routine of entire day
+
+day = input("Enter the day : ")
+time = float(input("Enter time : "))
+
+if(day=="saturday"):
+    print("learn revision of genearic subject and renew the trading chart")
+elif(day =="sunday"):
+    print("Learn and revision of operating system and operating system and learn about trading")
+elif(day=="monday"and(time>=10 and time<12)):
+    print("Revision 10am to 12pm")
+elif(day=="monday"and(time>=12 and time<15)):
+    print("DSA(Data structure and algorithms) 12pm to 3pm")
+elif(day=="monday"and(time>=15 and time<16)):
+    print("Rest and chill 3pm to 4pm")
+elif(day=="monday"and(time>=16 and time<19)):
+    print("AI(Artificial intelligence and machine learning) 4pm to 7pm")
+elif(day=="monday"and(time>=19 and time<20)):
+    print("Rest and chill 7pm to 8pm")
+elif(day=="monday"and(time>=20 and time<22)):
+    print("Trading time 8pm to 10pm")
+elif(day=="monday"and(time>=22 and time<23)):
+    print("Trading and eating food 10pm to 11pm ")
+elif(day=="monday"and(time>=23 and time<24.5)):
+    print("Youtube Knowledge 11pm to 12:30am")
+elif(day=="monday"and(time>=0.50 and time<2)):
+    print("Coding practice 12:30pm to 2:00am")
+elif(day=="monday"and(time>=2 and time<10)):
+    print("Rest and all other activity 2am to 10am")
+elif(day=="tuesday"and(time>=10 and time<12)):
+    print("Revision 10am to 12pm")
+elif(day=="tuesday"and(time>=12 and time<15)):
+    print("DSA(Data structure and algorithms) 12pm to 3pm")
+elif(day=="tuesday"and(time>=15 and time<16)):
+    print("Rest and chill 3pm to 4pm")
+elif(day=="tuesday"and(time>=16 and time<19)):
+    print("AI(Artificial intelligence and machine learning) 4pm to 7pm")
+elif(day=="tuesday"and(time>=19 and time<20)):
+    print("Rest and chill 7pm to 8pm")
+elif(day=="tuesday"and(time>=20 and time<22)):
+    print("Trading time 8pm to 10pm")
+elif(day=="tuesday"and(time>=22 and time<23)):
+    print("Trading and eating food 10pm to 11pm ")
+elif(day=="tuesday"and(time>=23 and time<24.5)):
+    print("Youtube Knowledge 11pm to 12:30am")
+elif(day=="tuesday"and(time>=0.50 and time<2)):
+    print("Coding practice 12:30pm to 2:00am")
+elif(day=="tuesday"and(time>=2 and time<10)):
+    print("Rest and all other activity 2am to 10am")
+elif(day=="wednusday"and(time>=10 and time<12)):
+    print("Revision 10am to 12pm")
+elif(day=="wednusday"and(time>=12 and time<15)):
+    print("DSA(Data structure and algorithms) 12pm to 3pm")
+elif(day=="wednusday"and(time>=15 and time<16)):
+    print("Rest and chill 3pm to 4pm")
+elif(day=="wednusday"and(time>=16 and time<19)):
+    print("AI(Artificial intelligence and machine learning) 4pm to 7pm")
+elif(day=="wednusday"and(time>=19 and time<20)):
+    print("Rest and chill 7pm to 8pm")
+elif(day=="wednusday"and(time>=20 and time<22)):
+    print("Trading time 8pm to 10pm")
+elif(day=="wednusday"and(time>=22 and time<23)):
+    print("Trading and eating food 10pm to 11pm ")
+elif(day=="wednusday"and(time>=23 and time<24.5)):
+    print("Youtube Knowledge 11pm to 12:30am")
+elif(day=="wednusday"and(time>=0.50 and time<2)):
+    print("Coding practice 12:30pm to 2:00am")
+elif(day=="wednusday"and(time>=2 and time<10)):
+    print("Rest and all other activity 2am to 10am")
+elif(day=="thrusday"and(time>=10 and time<12)):
+    print("Revision 10am to 12pm")
+elif(day=="thrusday"and(time>=12 and time<15)):
+    print("DSA(Data structure and algorithms) 12pm to 3pm")
+elif(day=="thrusday"and(time>=15 and time<16)):
+    print("Rest and chill 3pm to 4pm")
+elif(day=="thrusday"and(time>=16 and time<19)):
+    print("AI(Artificial intelligence and machine learning) 4pm to 7pm")
+elif(day=="thrusday"and(time>=19 and time<20)):
+    print("Rest and chill 7pm to 8pm")
+elif(day=="thrusday"and(time>=20 and time<22)):
+    print("Trading time 8pm to 10pm")
+elif(day=="thrusday"and(time>=22 and time<23)):
+    print("Trading and eating food 10pm to 11pm ")
+elif(day=="thrusday"and(time>=23 and time<24.5)):
+    print("Youtube Knowledge 11pm to 12:30am")
+elif(day=="thrusday"and(time>=0.50 and time<2)):
+    print("Coding practice 12:30pm to 2:00am")
+elif(day=="thrusday"and(time>=2 and time<10)):
+    print("Rest and all other activity 2am to 10am")
+elif(day=="friday"and(time>=10 and time<12)):
+    print("Revision 10am to 12pm")
+elif(day=="friday"and(time>=12 and time<15)):
+    print("DSA(Data structure and algorithms) 12pm to 3pm")
+elif(day=="friday"and(time>=15 and time<16)):
+    print("Rest and chill 3pm to 4pm")
+elif(day=="friday"and(time>=16 and time<19)):
+    print("AI(Artificial intelligence and machine learning) 4pm to 7pm")
+elif(day=="friday"and(time>=19 and time<20)):
+    print("Rest and chill 7pm to 8pm")
+elif(day=="friday"and(time>=20 and time<22)):
+    print("Trading time 8pm to 10pm")
+elif(day=="friday"and(time>=22 and time<23)):
+    print("Trading and eating food 10pm to 11pm ")
+elif(day=="friday"and(time>=23 and time<24.5)):
+    print("Youtube Knowledge 11pm to 12:30am")
+elif(day=="friday"and(time>=0.50 and time<2)):
+    print("Coding practice 12:30pm to 2:00am")
+elif(day=="friday"and(time>=2 and time<10)):
+    print("Rest and all other activity 2am to 10am")
